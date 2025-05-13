e9fb696d542f8a2cc8d43fa8fc52d9f174ba09df
1028ouo <shellyyi1028@gmail.com>
2025-05-13 23:44:05 +0800

    update package

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Pomodoro 登入',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.red),
